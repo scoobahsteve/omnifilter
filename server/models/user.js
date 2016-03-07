edbb1cdace139cc3a17c1cd8f9663b743564067a
ERIKAWHO <Erika@RefreshMediaResources.com>
2016-03-07 13:27:10 -0800

    Adding user setup

---
+++ server/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+var userSchema = new mongoose.Schema({
+  username: String,
+  authentication: {
+    email: String,
+    password: String,
+  }
+});
+
+userSchema.methods.hashPassword = function(password) {
+  var hash = this.authentication.password = bcrypt.hashSync(password, 8);
+  return hash;
+};
+
+userSchema.methods.comparePassword = function(password) {
+  return bcrypt.compareSync(password, this.authentication.password);
+};
