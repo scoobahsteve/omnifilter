7992cb239c15995ff5516837a8f89e6b8529d524
Gene Troy <genetroy@hotmail.com>
2016-03-07 15:22:27 -0800

    add user controller

---
+++ app/js/users/controllers/users_controller.js
@@ -0,0 +1,55 @@
+var angular = require('angular');
+
+module.exports = function(app) {
+  app.controller('UsersController', ['$scope', '$http', 'cfResource', 'cfStore',
+  function($scope, $http, Resource, cfStore) {
+    //These are recycled lines from Tyler
+      //$scope.greeting = 'hello world';
+      //$scope.fakeUser={name: 'a fake user', fishPreference: 'suffering'};
+      //cfStore.set('greeting', 'hello world');
+    $scope.users = [];
+    var userService = Resource('/users');
+
+    $scope.toggleEdit = function(user) {
+      if (user.backup) {
+        var temp = user.backup;
+        $scope.users.splice($scope.users.indexOf(user), 1, temp);
+      } else {
+        user.backup = angular.copy(user);
+        user.editing = true;
+      }
+    };
+
+    $scope.getAll = function() {
+      userService.getAll(function(err, res) {
+        if (err) return console.log(err);
+        $scope.users = res;
+      });
+    };
+
+    $scope.createUser = function(user) {
+      $scope.users.push(user);
+      userService.create(user, function(err, res) {
+        if (err) return console.log(err);
+        $scope.users.splice($scope.users.indexOf(user), 1, res);
+        $scope.newUser = null;
+      });
+    };
+
+    $scope.deleteUser = function(user) {
+      if (!user._id) return setTimeout(function() {$scope.deleteUser(user);}, 1000);
+      userService.delete(user, function(err, res) {
+        if (err) return console.log(err);
+        $scope.users.splice($scope.users.indexOf(user), 1);
+      });
+    };
+
+    $scope.updateUser = function(user) {
+      userService.update(user, function(err, res) {
+        user.editing = false;
+        user.backup = null;
+        if (err) return console.log(err);
+      });
+    };
+  }]);
+};
