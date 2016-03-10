905dfe4e9fff012d8cfbf8db85169c12026fc201
Stephen Salzer <stephen.salzer@gmail.com>
2016-03-10 10:00:04 -0800

    formatting the testages

--- server/test/user_routes_test.js
+++ server/test/user_routes_test.js
@@ -12,28 +12,30 @@ require(__dirname + '/../server');
 const User = require(__dirname + '/../models/user');
 const Content = require(__dirname + '/../models/content');
 var userToken;
+var testUser;
 
-describe('user API', () => {
+describe('user API POST method', () => {
+
   before((done) => {
-    var newUser = new User();
-   newUser.email = 'gene@gmail.com';
-   newUser.hashPassword('password');
-   newUser.save( (err, data) => {
-     userToken = data.generateToken();
+    testUser = new User();
+    testUser.email = 'gene@gmail.com';
+    testUser.hashPassword('password');
+    testUser.save( (err, data) => {
+    testUser.token = userToken = data.generateToken();
     done();
-  });
-});
-
-  after((done) => {
-    User.remove({}, function(err) {
-      Content.remove({}, function(err) {
-        done();
-      });
     });
   });
 
+  after((done) => {
+    User.remove({}, function(err) {
+    });
+    Content.remove({}, function(err) {
+    });
+    done();
+  });
+
   it('should create a user with a POST', (done) => {
-      request('localhost:3000')
+    request('localhost:3000')
         .post('/newuser')
         .set('token', userToken)
         .send({ user: 'test user' })
@@ -42,11 +44,11 @@ describe('user API', () => {
           expect(res).to.have.status(200);
           expect(res.body.user).to.eql('test user');
           expect(res.body).to.have.property('user_id');
-      });
-      done();
+        });
+    done();
   });
 
-
+describe('check if user exists', () => {
   it('should be able to verify that a user exists', (done) => {
     request('localhost:3000')
       .get('/verify/')
@@ -57,39 +59,33 @@ describe('user API', () => {
         done();
       });
     });
+});
 
-describe('tests that require a user in db', (done) => {
-  beforeEach((done) => {
-    User.create( { email: 'gene@gmail.com', password: 'password' }, (err, data) => {
-      if (err) return console.log(err);
-        this.testUser = data;
+describe('ability to UPDATE and DELETE', () => {
+  it('should be able to UPDATE a user', (done) => {
+    request ('localhost:3000')
+      .put('/usersettings/' + testUser._id)
+      .set('token', userToken)
+      .send({ email: 'new email' })
+      .end(function (err, res) {
+        expect(err).to.eql(null);
+        expect(res.body.msg).to.eql('User updated');
+        expect(res).to.have.status(200);
         done();
-        });
       });
+    });
 
-      it('should be able to UPDATE a user', (done) => {
-        request('localhost:3000')
-          .put('/usersettings/' + this.testUser._id)
-          .set('token', userToken)
-          .send({ email: 'new email' })
-          .end(function (err, res) {
-            expect(err).to.eql(null);
-            expect(res.body.msg).to.eql('User updated');
-            expect(res).to.have.status(200);
-            done();
-          });
-      });
-
-      it('should be able to DELETE a user', (done) => {
-        request('localhost:3000')
-          .delete('/deleteuser/' + this.testUser._id)
-          .set('token', userToken)
-          .end((err, res) => {
-            expect(err).to.eql(null);
-            expect(res.body.msg).to.eql('User deleted');
-            expect(res).to.have.status(200);
-            done();
-          });
+  it('should be able to DELETE a user', (done) => {
+    request('localhost:3000')
+      .delete('/deleteuser/' + testUser._id)
+      .set('token', userToken)
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res.body.msg).to.eql('User deleted');
+        expect(res).to.have.status(200);
+        done();
       });
     });
   });
+});
+
